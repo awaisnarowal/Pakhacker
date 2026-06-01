d13b3ad2fcb24f5053345ea96c1aac666ffc5c2c
awaisnarowal <147997652+awaisnarowal@users.noreply.github.com>
2026-06-02 02:13:00 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,17 @@
-const CACHE_NAME = 'pakhacker-cache-v4.3.1';
+const CACHE_NAME = "pakhackerpro-v3"; // Version update
+const ASSETS = ["./", "./index.html", "./home.html", "./manifest.json", "./icon/icon-192x192.png", "./icon/icon-512x512.png"];
 
-self.addEventListener('install', (event) => {
-    self.skipWaiting();
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames.map((cache) => {
-                    if (cache !== CACHE_NAME) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        })
-    );
-    self.clients.claim();
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((key) => {
+    if (key !== CACHE_NAME) return caches.delete(key);
+  }))).then(() => self.clients.claim()));
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        fetch(event.request).catch(() => {
-            return caches.match(event.request);
-        })
-    );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
