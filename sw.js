4fdfae48a12c5ce082e1f13ba6511437b523dc2f
awaisnarowal <147997652+awaisnarowal@users.noreply.github.com>
2026-06-02 02:32:18 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,44 @@
-const CACHE_NAME = "pakhackerpro-v3"; // Version update
-const ASSETS = ["./", "./index.html", "./home.html", "./manifest.json", "./icon/icon-192x192.png", "./icon/icon-512x512.png"];
+const CACHE_NAME = "pakhackerpro-v4";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./home.html",
+  "./manifest.json",
+  "./icons/icon-192x192.png",
+  "./icons/icon-512x512.png"
+];
 
 self.addEventListener("install", (e) => {
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
 self.addEventListener("activate", (e) => {
-  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((key) => {
-    if (key !== CACHE_NAME) return caches.delete(key);
-  }))).then(() => self.clients.claim()));
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request).catch(() => {
+        if (e.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
+    })
+  );
 });
